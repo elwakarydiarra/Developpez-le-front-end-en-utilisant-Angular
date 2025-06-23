9c12c21b398e3d108d336653f9c93c29858fd2ed
el hadji diarra <wakary.diarra@gmail.com>
2025-06-23 23:29:48 +0200

    page country

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
-import { DetailComponent } from './country-details/country-details.component';
+import { DetailComponent } from './pages/country-details/country-details.component';
 
 
 const routes: Routes = [
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
-import { DetailComponent } from './country-details/country-details.component';
+import { DetailComponent } from './pages/country-details/country-details.component';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { DashboardComponent } from './pages/home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
